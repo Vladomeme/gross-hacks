e63185e96ed9ad8d220eb626216cc76f129922eb
Vladomeme <84517135+Vladomeme@users.noreply.github.com>
2023-05-18 16:41:33 +0500

    Add files via upload

--- src/main/java/net/grosshacks/main/GrossHacksConfigScreen.java
+++ src/main/java/net/grosshacks/main/GrossHacksConfigScreen.java
@@ -64,11 +64,11 @@ public class GrossHacksConfigScreen {
                         .setDefaultValue(defaultConfig.remove_interactions)
                         .build());
 
-                category.addEntry(entryBuilder.startBooleanToggle(Text.of("Hide entities in spawners"), currentConfig.break_spawners)
-                        .setTooltip(Text.of("Hides the spinny mobs in spawners. Helps with performance,\n" +
-                                "especially in spawner heavy areas like Quelled Convent."))
-                        .setSaveConsumer(newConfig -> currentConfig.break_spawners = newConfig)
-                        .setDefaultValue(defaultConfig.break_spawners)
+                category.addEntry(entryBuilder.startBooleanToggle(Text.of("Spawner entity culling"), currentConfig.spawner_culling)
+                        .setTooltip(Text.of("Hides the spinny mobs in inactive spawners. Helps with\n" +
+                                "performance in spawner heavy areas."))
+                        .setSaveConsumer(newConfig -> currentConfig.spawner_culling = newConfig)
+                        .setDefaultValue(defaultConfig.spawner_culling)
                         .build());
 
                 category.addEntry(entryBuilder.startBooleanToggle(Text.of("Hide handheld items on players"), currentConfig.hide_handheld)
--- src/main/java/net/grosshacks/main/mixin/MobSpawnerBlockEntityRendererMixin.java
+++ src/main/java/net/grosshacks/main/mixin/MobSpawnerBlockEntityRendererMixin.java
@@ -1,25 +1,24 @@
 package net.grosshacks.main.mixin;
 
 import net.grosshacks.main.GrossHacksConfig;
+import net.minecraft.block.entity.MobSpawnerBlockEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.MobSpawnerBlockEntityRenderer;
-import net.minecraft.entity.Entity;
-import net.minecraft.world.MobSpawnerLogic;
-import net.minecraft.world.World;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MobSpawnerBlockEntityRenderer.class)
 public class MobSpawnerBlockEntityRendererMixin {
 
-	@Redirect(method = "render(Lnet/minecraft/block/entity/MobSpawnerBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/MobSpawnerLogic;getRenderedEntity(Lnet/minecraft/world/World;)Lnet/minecraft/entity/Entity;"))
-	private Entity render(MobSpawnerLogic instance, World world) {
-		if(GrossHacksConfig.INSTANCE.break_spawners){
-			return null;
-		}
-		else {
-			return instance.getRenderedEntity(world);
+	@Inject(method = "render(Lnet/minecraft/block/entity/MobSpawnerBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V",
+			at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/entity/MobSpawnerBlockEntity;getLogic()Lnet/minecraft/world/MobSpawnerLogic;"), cancellable = true)
+	private void render(MobSpawnerBlockEntity mobSpawnerBlockEntity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j, CallbackInfo ci) {
+		if(GrossHacksConfig.INSTANCE.spawner_culling && !mobSpawnerBlockEntity.getLogic().isPlayerInRange(mobSpawnerBlockEntity.getWorld(), mobSpawnerBlockEntity.getPos())){
+			matrixStack.pop();
+			ci.cancel();
 		}
 	}
 }
