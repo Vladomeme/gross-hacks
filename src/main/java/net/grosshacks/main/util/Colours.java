bebfb1b1ff2d2107c3a546288a55c000c95eecf4
Vladomeme <vladomen04@gmail.com>
2025-02-15 16:13:58 +0500

    god damn

--- src/main/java/net/grosshacks/main/util/Colours.java
+++ src/main/java/net/grosshacks/main/util/Colours.java
@@ -5,27 +5,25 @@ import net.handbook.main.config.HandbookConfig;
 
 public class Colours {
 
-    private static final HandbookConfig config = HandbookConfig.INSTANCE;
-
     public static int text() {
-        return GrossHacks.handbookAvailable ? config.textColor : -1;
+        return GrossHacks.handbookAvailable ? HandbookConfig.INSTANCE.textColor : -1;
     }
 
     public static int background(boolean isMouseOver) {
         if (GrossHacks.handbookAvailable)
-            return isMouseOver ? config.highlightColor : config.tradeBackgroundColor;
+            return isMouseOver ? HandbookConfig.INSTANCE.highlightColor : HandbookConfig.INSTANCE.tradeBackgroundColor;
         else return isMouseOver ? 1688906410 : 866822826;
     }
 
     public static int backgroundAlt() {
-        return GrossHacks.handbookAvailable ? config.screenHeadColor : 548055807;
+        return GrossHacks.handbookAvailable ? HandbookConfig.INSTANCE.screenHeadColor : 548055807;
     }
 
     public static int border() {
-        return GrossHacks.handbookAvailable ? config.bordersColor : -1;
+        return GrossHacks.handbookAvailable ? HandbookConfig.INSTANCE.bordersColor : -1;
     }
 
     public static int highlight() {
-        return GrossHacks.handbookAvailable ? config.favouriteColor : 2030023680;
+        return GrossHacks.handbookAvailable ? HandbookConfig.INSTANCE.favouriteColor : 2030023680;
     }
 }
