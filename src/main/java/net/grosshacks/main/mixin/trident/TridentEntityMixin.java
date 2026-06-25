d89cfcd9f3f23cf377896845b6536624d6943eb7
Vladomeme <vladomen04@gmail.com>
2026-06-25 14:43:22 +0500

    singleplayer race fix

--- src/main/java/net/grosshacks/main/mixin/trident/TridentEntityMixin.java
+++ src/main/java/net/grosshacks/main/mixin/trident/TridentEntityMixin.java
@@ -45,7 +45,8 @@ public abstract class TridentEntityMixin extends PersistentProjectileEntity impl
         }
 
         ItemStack item;
-        PlayerEntity nearestPlayer = Objects.requireNonNull(MinecraftClient.getInstance().player);
+        PlayerEntity nearestPlayer = MinecraftClient.getInstance().player;
+        if (nearestPlayer == null) return;
 
         if (getOwner() != null) item = getOwner().getHandItems().iterator().next();
         else {
